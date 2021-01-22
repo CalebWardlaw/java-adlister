b7be3904af8318f1f7a9c7dc9ac506d4c3fc9555
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-22 10:23:14 -0600

    created quotes bean

---
+++ src/main/java/models/Quotes.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Quotes {
+
+    private String author_first_name;
+    private String author_last_name;
+    private String content;
+
+    public Quotes(){}
+
+    public String getAuthorFirstName(){
+        return author_first_name;
+    }
+
+    public void setAuthorFirstName(String author_first_name){
+        this.author_first_name = author_first_name;
+    }
+
+    public String getAuthorLastName(){
+        return author_last_name;
+    }
+
+    public void setAuthorLastName(String author_last_name){
+        this.author_last_name = author_last_name;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String setContent(){
+        this.content = content;
+    }
+}
