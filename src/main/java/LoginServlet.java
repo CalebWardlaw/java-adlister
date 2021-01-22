36f9fab850f7dbe972e6698573b7ab019176c58c
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-22 15:52:48 -0600

    created login servlet to seperate logic from jsp

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//  req.getRequestDispatcher - sends out request, and forwards it to/login
+        req.getRequestDispatcher("/login.jsp").forward(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+
+        if (validAttempt) {
+            res.sendRedirect("/profile");
+        } else {
+            res.sendRedirect("/login");
+        }
+    }
+
+
+}
