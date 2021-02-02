108831b52dd558fca4305dc4a947701af2a19cbf
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-02-01 20:40:41 -0600

    end of day commit

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -18,13 +18,14 @@ public class CreateAdServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
-
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
             .forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         User user = (User) request.getSession().getAttribute("user");
+
         Ad ad = new Ad(
                 user.getId(),
                 request.getParameter("title"),
